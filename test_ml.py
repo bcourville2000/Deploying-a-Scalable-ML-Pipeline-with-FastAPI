d579fa5618d5fa539179c2024cd8a663631d347a
bcourville2000 <bcourville2000@gmail.com>
2024-10-28 12:13:44 -0600

    completed test_ml.py and uploaded screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,76 @@
 import pytest
-# TODO: add necessary import
+import pickle
+import pandas as pd
+from ml.model import compute_model_metrics
+from train_model import y_test, preds
+from sklearn.ensemble import RandomForestClassifier
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_algo_type():
     """
-    # add description for the first test
+    Simple test to check and ensure the algo type is the one specified
     """
-    # Your code here
-    pass
+    # Expected algorithm type. replace as necessary with the expected type 
+    algorithm_type = RandomForestClassifier #change as necessary
 
+    # Load pickle file with model.
+    with open("model/model.pkl", "rb") as f:
+        model = pickle.load(f)
+    
+    # Check if model matches expected algorithm type.
+    assert isinstance(model, algorithm_type), print(f"expected algorithm: {algorithm_type} model algorithm: {type(model)}")
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_metrics():
     """
-    # add description for the second test
+    test to ensure the metrics produced are the expected ones for repeatability.
     """
-    # Your code here
-    pass
+    # compute metric values
+    metrics = compute_model_metrics(y_test, preds)
 
+    # round metric values to 3 places
+    metrics = [round(i, 3) for i in metrics]
+    
+    #define expected values
+    prec = 0.723 # Precision value produced by compute_model_metrics.
+    rec = 0.645 # Recall value produced by compute_model_metrics.
+    f1 = 0.682 # F1 value produced by compute_model_metrics.
+    exp_vals = [prec, rec, f1]
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+    #compare metrics with expected values
+    
+    assert exp_vals == metrics, print(f"The expected metric value:{i} was not in the list of metrics:{metrics}")
+
+def test_data_verify():
     """
-    # add description for the third test
+    ensure the dataset contains the correct number of columns and the correct names for columns.
     """
-    # Your code here
-    pass
+    # define expected values in dataset
+    num_cols = 15 # expected number of columns 
+    csv_cols = [ #expected column names
+        'age',
+        'workclass',
+        'fnlgt',
+        'education',
+        'education-num',
+        'marital-status',
+        'occupation',
+        'relationship',
+        'race',
+        'sex',
+        'capital-gain',
+        'capital-loss',
+        'hours-per-week',
+        'native-country',
+        'salary'
+        ]
+    
+    #load csv
+    df = pd.read_csv("data/census.csv")
+
+    # compare number of columns
+    assert len(df.columns) == num_cols, print(f"Expected number of columns: {num_cols} actual number of columns: {len(df.columns)}")
+
+    #create list of column names from csv
+    df_cols = list(df.columns.values)
+
+    #compare column name lists
+    assert df_cols == csv_cols, print("Column names do not match expected values!!!")
